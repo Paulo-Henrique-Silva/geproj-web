45fcbfc7c57e96ab4c68026b65e8b12febd75001
Paulo <pshenrique2015@gmail.com>
2025-01-02 21:51:04 -0300

    Creating the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,43 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+import { definePreset } from '@primeng/themes';
+
+const MyPreset = definePreset(Lara, {
+  semantic: {
+    //Primary Color
+    primary: {
+      50: '{green.50}',
+      100: '{green.100}',
+      200: '{green.200}',
+      300: '{green.300}',
+      400: '{green.400}',
+      500: '{green.500}',
+      600: '{green.600}',
+      700: '{green.700}',
+      800: '{green.800}',
+      900: '{green.900}',
+      950: '{green.950}'
+    }
+  }
+});
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: MyPreset,
+                options: {
+                    darkModeSelector: '.my-app-dark'
+                }
+            }
+        })
+  ]
 };
